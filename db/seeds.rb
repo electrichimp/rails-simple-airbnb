10f36fb482880435469cea5efdb653a4ccd0896c
JUAN MANUEL GOMEZ <jumagom@gmail.com>
2022-01-19 17:02:14 -0500

    ya ta

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Flat.destroy_all
+
+10.times do
+  Flat.create(name: Faker::Address.street_address,
+              address: "#{Faker::Address.city} - #{Faker::Address.country}",
+              description: Faker::Lorem.sentence,
+              price_per_night: rand(50..500),
+              number_of_guests: rand(1..5),
+              image_url: "https://source.unsplash.com/random/500x300/?airbnb/#{rand(1..20)}")
+end
